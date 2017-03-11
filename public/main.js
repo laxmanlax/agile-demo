81c70444b3a1d31034227c6975c264ac9ce7993d
Dimitry Solovyov <dimitry@agilestacks.com>
2017-03-11 16:55:31 +0200

    Show app and version for non-update-demo pods

--- public/main.js
+++ public/main.js
@@ -4,6 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const templateRequest = axios.get('/template.hbs')
         .then(({data}) => Handlebars.compile(data));
 
+    function handleClusterItems() {
+        document.querySelectorAll('.cluster-item').forEach((item) => {
+            const image = item.querySelector('img');
+            image.addEventListener('error', () => {
+                item.classList.add('cluster-item-with-header');
+            });
+        });
+    }
+
     let model;
     function refresh() {
         Promise.all([templateRequest, axios.get('/cluster')])
@@ -11,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (!_.isEqual(model, data.items)) {
                     model = data.items;
                     container.innerHTML = template(model);
+                    handleClusterItems();
                 }
             });
     }
